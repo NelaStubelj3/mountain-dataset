cffe7bf892b9ef06f9409ae6038e0c844633c8aa
NelaStubelj3 <ns53293@fer.hr>
2023-11-02 14:17:40 +0100

    Removed mountain huts, corrected license and database to meet lab requirements

--- README.md
+++ README.md
@@ -1,37 +1,30 @@
 # mountain-dataset
+
 Mountain dataset for Open Computing course
 
- ## Metadata
- - **License**: Creative Commons CC0 1.0 License
- - **Author**: Nela Štubelj
- - **Version**: 1.0
- - **Language**: English
- - **Attributes**: 
-	1. 'mountain_id': Identifier of the mountain.
-	2. 'mountain_name': Name of the mountain.
-	3. 'country': Country where the mountain is located.
-	4. 'height': Height of the mountain.
-	5. 'mountain_description': Description of the mountain.
-	6. 'highest_peak': Name of the highest peak on the mountain.
-	7. 'trail_id': Unique identifier for trails.
-	8. 'trail_name': Name of the trail.
-	9. 'trail_description': Description of the trail.
-	10. 'difficulty': Difficulty level of the hiking trail.
-	11. 'length': Length of the hiking trail.
-	12. 'elevation_gain': Elevation gain on the hiking trail.
-	13. 'mountain_id': Identifier of the associated mountain.
-	14. 'hut_id': Identifier for the mountain hut.
-	15. 'hut_name': Name of the mountain hut.
-	16. 'address': Address of the mountain hut.
-	17. 'capacity': Capacity of the mountain hut.
+## Metadata
 
+- **License**: Creative Commons CC0 1.0 License
+- **Author**: Nela Štubelj
+- **Version**: 1.0
+- **Language**: English
+- **Attributes**:
+  1.  'mountain_id': Identifier of the mountain.
+  2.  'mountain_name': Name of the mountain.
+  3.  'mountain_country': Country where the mountain is located.
+  4.  'mountain_height': Height of the mountain.
+  5.  'mountain_description': Description of the mountain.
+  6.  'mountain_highest_peak': Name of the highest peak on the mountain.
+  7.  'trail_id': Unique identifier for trails.
+  8.  'trail_name': Name of the trail.
+  9.  'trail_description': Description of the trail.
+  10. 'trail_difficulty': Difficulty level of the hiking trail.
+  11. 'trail_length': Length of the hiking trail.
+  12. 'trail_elevation_gain': Elevation gain on the hiking trail.
 
+## Repository Structure
 
- ## Repository Structure
- 
- - 'mountains.json': JSON file containing information about mountains.
- - 'mountains.csv': CSV file containing information about mountains.
+- 'mountains.json': JSON file containing information about mountains.
+- 'mountains.csv': CSV file containing information about mountains.
 
- - 'README.md': This file with dataset description.
- 
-	
+- 'README.md': This file with dataset description.
--- database_dump.sql
+++ database_dump.sql
@@ -16,6 +16,32 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: combined_data; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.combined_data (
+    mountain_id integer,
+    mountain_name character varying(255),
+    mountain_country character varying(255),
+    mountain_height numeric(7,2),
+    mountain_description text,
+    mountain_highest_peak character varying,
+    trail_id integer,
+    trail_name character varying(255),
+    trail_description text,
+    trail_difficulty character varying(50),
+    trail_length numeric(5,1),
+    trail_elevation_gain numeric(5,1)
+);
+
+
+ALTER TABLE public.combined_data OWNER TO postgres;
+
 --
 -- Name: hut_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
@@ -29,47 +55,6 @@ CREATE SEQUENCE public.hut_id_seq
 
 
 ALTER TABLE public.hut_id_seq OWNER TO postgres;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: mountainhuts; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.mountainhuts (
-    hut_id integer NOT NULL,
-    hut_name character varying(255) NOT NULL,
-    address character varying(255),
-    capacity integer,
-    mountain_id integer
-);
-
-
-ALTER TABLE public.mountainhuts OWNER TO postgres;
-
---
--- Name: mountainhuts_hutid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.mountainhuts_hutid_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.mountainhuts_hutid_seq OWNER TO postgres;
-
---
--- Name: mountainhuts_hutid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.mountainhuts_hutid_seq OWNED BY public.mountainhuts.hut_id;
-
 
 --
 -- Name: mountains; Type: TABLE; Schema: public; Owner: postgres
@@ -177,13 +162,6 @@ ALTER SEQUENCE public.trails_trailid_seq OWNED BY public.trails.trail_id;
 
 
 --
--- Name: mountainhuts hut_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.mountainhuts ALTER COLUMN hut_id SET DEFAULT nextval('public.mountainhuts_hutid_seq'::regclass);
-
-
---
 -- Name: mountains mountain_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -191,20 +169,20 @@ ALTER TABLE ONLY public.mountains ALTER COLUMN mountain_id SET DEFAULT nextval('
 
 
 --
--- Data for Name: mountainhuts; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: combined_data; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.mountainhuts (hut_id, hut_name, address, capacity, mountain_id) FROM stdin;
-5	Mandara hut	Kilimanjaro National Park	60	3
-6	Horombo Hut	Kilimanjaro National Park	120	3
-7	Charles Inglis Clark Memorial Hut	Ben Nevis	26	4
-3	Domhutte SAC	Salome Keller 3928   Randa	9	2
-8	Snaefellsskali Hut	Vatnajokull National Park	45	6
-9	Mountain Hut Zavizan	Velebit	29	10
-10	Mountain Hut Vosac	Biokovo	25	8
-4	Edelweiss	Erich und Philippa Hischier und Short 3920 Zermatt	40	2
-1	Refuge de Bellachat Mountain Hut	305 Route de Vers le Nant Chamonix	24	1
-2	The Tete Rousse Refuge mountain hut	82 Rue du Mont Blanc Saint Gervais	72	1
+COPY public.combined_data (mountain_id, mountain_name, mountain_country, mountain_height, mountain_description, mountain_highest_peak, trail_id, trail_name, trail_description, trail_difficulty, trail_length, trail_elevation_gain) FROM stdin;
+1	Mont Blanc	France	4810.45	The highest mountain in the Alps	Mont Blanc de Courmayeur	1	La Visaille - Peuterey	A pleasant excursion in the mountains of Courmayeur, which starts from La Visaille and proceeds along the Zerotta road. The route reaches the locality of Peuterey passing through the Mont Blanc Refuge (1680m), Peindeint, Lassy Damon, Lassy Desot and Perthud.	Easy	6.0	153.0
+2	Matterhorn	Switzerland	4478.45	Famous pyramid-shaped mountain	Dufourspitze	3	Matterhorn Glacier Trail: Schwarzsee - Matterhorngletscher - Trockener Steg	This trail is a high-altitude route through the Alpine peaks, leading from Schwarzsee to Trockener Steg and offering fantastic views of the Matterhorn Glacier. The starting point can be reached with the Matterhorn Express cable car. It's quite a popular trail with amazing terrain. Very rocky in some places, so good footwear is recommended. Please be aware that the trail can be completely covered in snow starting from October.	Hard	6.3	498.0
+3	Kilimanjaro	Tanzania	5895.16	Tallest freestanding mountain in the world	Uhuru Peak	4	Mount Kilimanjaro: Machame Route	This is the route to climb Kilimanjaro via Machame route. Seven day trip, camping on the mountain six nights. 	Hard	57.0	5185.0
+4	Ben Nevis	United Kingdom	1344.53	Highest peak in the UK	Ben Nevis	5	Ben Nevis Mountain Track	The UK's highest mountain, "The Ben" or Ben Nevis, is one of the most popular spots in Scotland. A challenging and sustained climb, this route will test your endurance as you wind your way up the rocky and steep track to the summit. This big climb must not be underestimated and the correct preparation is required for a safe day out.Please note, due to the high elevation gain, the summit is frequently very cold, windy, and wet and you will always need to be prepared for changeable weather and freezing conditions, even in summer. This includes snow and ice that can often remain on this route beyond winter. It is therefore necessary to check weather and summit conditions before heading out as the temperate difference between the trailhead and the top drops significantly. Warm and waterproof layers, plenty of food and water, sun protection, and sturdy footwear are all essential for this trail. 	Hard	15.9	1342.0
+5	Etna	Italy	3330.99	Europe's most active volcano	Punta La Montagnola	6	Pian del Lago Crater	SAFETY NOTICE: The Etna Volcano is active and constantly erupting. It is therefore advisable to check the weather and geological forecasts and the state of the trails before walking in this area. For more information: https://www.ct.ingv.it/ https://parcoetna.it/. A pleasant excursion to one of the craters of Etna, starting from the Montagnola, reachable by cable car from the Sapienza Refuge. The route takes place in a loop and reaches the Pian del Lago Crater which was formed during the eruption of 2001. From the top of the crater it is possible to see the Barbagallo Crater and the Summit Craters.	Moderate	2.1	158.0
+7	Dinara	Croatia	1831.97	Prominent mountain on the border with Bosnia and Herzegovina	Troglav	8	Sveti Jakov - Troglav - Velika Duvjakusa	This demanding trail starts from the Sveti Jakov mountain house (1001 m), which is located on the edge of Ravni Vrdov. The trail leads to the peak of Troglav (1913 m), which is located in the territory of Bosnia and Herzegovina and is the highest peak of the Dinara mountain range in the Dinaric Mountains. From Troglav, the trail leads to the top of Velika Duvjakusa (1708 m), the highest peak on the Croatian side. Below Velika Duvjakusa is the Puma mountain shelter.	Hard	32.5	1517.0
+8	Biokovo	Croatia	1762.14	Stunning coastal mountain in southern Croatia	Sveti Jure	9	Makar - Vosac	This trail starts from the village of Makar, located right above Makarska. It is the oldest hiking trail on the southern side of Biokovo, the largest, longest, most beautiful, but also the harshest mountain in Dalmatia. The path normally leads to the peak of Sveti Jure (1594 m), the highest peak of Biokovo, and our path goes to the peak of Vosac (1422 m). At the very beginning, the trail is wide and leads along serpentines, winding continuously. The higher the trail climbs, the narrower it gets. On the top of Vosac is the Toni Roso Mountain Hut, from where you can enjoy a wonderful view in all directions.	Hard	10.6	1191.0
+6	Vatnajokull	Iceland	2110.13	Largest ice cap in Europe	Hvannadalshnjukur	7	Svartifoss - Sjonarsker	This fantastic trail will show you several waterfalls in Skaftafell. First you go in direction of Lambhagi to reach the Svartifoss waterfall. Svartifoss is surrounded by dark lava columns which gave rise to his name. Fantastic basalt Columnar next to the waterfall. After that you will see a second waterfall called Magnusarfoss and it's the smallest of the 3 waterfalls and a last one called Hundafoss, which is the tallest. The path then goes to the Sjonarsker mountain that offers you incredible views.	Moderate	4.7	193.0
+9	Ucka	Croatia	1401.63	Mountain range overlooking the Istrian Peninsula	Vojak	10	Mala Ucka - Vojak	The trail starts from the village of Mala Ucka, the highest rural settlement in Istria, located at 995 meters above sea level. The village is almost deserted. The path continues through a tall beech forest with a sharp climb to the exit to the saddle at the foot of the Vojak peak, from where there is a sharp ascent along a grassy rocky ridge all the way to the top where the old lookout tower is located, offering a beautiful view in all directions. The return goes to the saddle and the circular path back to Mala Ucka.	Moderate	6.9	429.0
+1	Mont Blanc	France	4810.45	The highest mountain in the Alps	Mont Blanc de Courmayeur	2	The Mont Blanc Tour, Segment 4	The Mont Blanc Tour crosses three countries in 170 km of discovery for a total of 10 days of satisfaction. This route is a variant of Segment 4, starting from the Combal Refuge (1969m) and proceeding towards Lake Miage before returning to the trail and approaching Courmayeur. The route takes place with steep paths but beautiful views. Some sections of the trail are eroded, so caution is advised.	Moderate	13.4	560.0
 \.
 
 
@@ -232,13 +210,13 @@ COPY public.mountains (mountain_id, mountain_name, country, height, mountain_des
 
 COPY public.trails (trail_id, trail_name, trail_description, difficulty, length, elevation_gain, mountain_id) FROM stdin;
 1	La Visaille - Peuterey	A pleasant excursion in the mountains of Courmayeur, which starts from La Visaille and proceeds along the Zerotta road. The route reaches the locality of Peuterey passing through the Mont Blanc Refuge (1680m), Peindeint, Lassy Damon, Lassy Desot and Perthud.	Easy	6.0	153.0	1
-2	The Mont Blanc Tour, Segment 4	The Mont Blanc Tour crosses three countries in 170 km of discovery for a total of 10 days of satisfaction.\nThis route is a variant of Segment 4, starting from the Combal Refuge (1969m) and proceeding towards Lake Miage before returning to the trail and approaching Courmayeur. The route takes place with steep paths but beautiful views. Some sections of the trail are eroded, so caution is advised.	Moderate	13.4	560.0	1
 3	Matterhorn Glacier Trail: Schwarzsee - Matterhorngletscher - Trockener Steg	This trail is a high-altitude route through the Alpine peaks, leading from Schwarzsee to Trockener Steg and offering fantastic views of the Matterhorn Glacier. The starting point can be reached with the Matterhorn Express cable car. It's quite a popular trail with amazing terrain. Very rocky in some places, so good footwear is recommended. Please be aware that the trail can be completely covered in snow starting from October.	Hard	6.3	498.0	2
 4	Mount Kilimanjaro: Machame Route	This is the route to climb Kilimanjaro via Machame route. Seven day trip, camping on the mountain six nights. 	Hard	57.0	5185.0	3
 6	Pian del Lago Crater	SAFETY NOTICE: The Etna Volcano is active and constantly erupting. It is therefore advisable to check the weather and geological forecasts and the state of the trails before walking in this area. For more information: https://www.ct.ingv.it/ https://parcoetna.it/. A pleasant excursion to one of the craters of Etna, starting from the Montagnola, reachable by cable car from the Sapienza Refuge. The route takes place in a loop and reaches the Pian del Lago Crater which was formed during the eruption of 2001. From the top of the crater it is possible to see the Barbagallo Crater and the Summit Craters.	Moderate	2.1	158.0	5
 5	Ben Nevis Mountain Track	The UK's highest mountain, "The Ben" or Ben Nevis, is one of the most popular spots in Scotland. A challenging and sustained climb, this route will test your endurance as you wind your way up the rocky and steep track to the summit. This big climb must not be underestimated and the correct preparation is required for a safe day out.Please note, due to the high elevation gain, the summit is frequently very cold, windy, and wet and you will always need to be prepared for changeable weather and freezing conditions, even in summer. This includes snow and ice that can often remain on this route beyond winter. It is therefore necessary to check weather and summit conditions before heading out as the temperate difference between the trailhead and the top drops significantly. Warm and waterproof layers, plenty of food and water, sun protection, and sturdy footwear are all essential for this trail. 	Hard	15.9	1342.0	4
 7	Svartifoss - Sjonarsker	This fantastic trail will show you several waterfalls in Skaftafell. First you go in direction of Lambhagi to reach the Svartifoss waterfall. Svartifoss is surrounded by dark lava columns which gave rise to his name. Fantastic basalt Columnar next to the waterfall. After that you will see a second waterfall called Magnusarfoss and it's the smallest of the 3 waterfalls and a last one called Hundafoss, which is the tallest. The path then goes to the Sjonarsker mountain that offers you incredible views.	Moderate	4.7	193.0	6
 8	Sveti Jakov - Troglav - Velika Duvjakusa	This demanding trail starts from the Sveti Jakov mountain house (1001 m), which is located on the edge of Ravni Vrdov. The trail leads to the peak of Troglav (1913 m), which is located in the territory of Bosnia and Herzegovina and is the highest peak of the Dinara mountain range in the Dinaric Mountains. From Troglav, the trail leads to the top of Velika Duvjakusa (1708 m), the highest peak on the Croatian side. Below Velika Duvjakusa is the Puma mountain shelter.	Hard	32.5	1517.0	7
+2	The Mont Blanc Tour, Segment 4	The Mont Blanc Tour crosses three countries in 170 km of discovery for a total of 10 days of satisfaction. This route is a variant of Segment 4, starting from the Combal Refuge (1969m) and proceeding towards Lake Miage before returning to the trail and approaching Courmayeur. The route takes place with steep paths but beautiful views. Some sections of the trail are eroded, so caution is advised.	Moderate	13.4	560.0	1
 9	Makar - Vosac	This trail starts from the village of Makar, located right above Makarska. It is the oldest hiking trail on the southern side of Biokovo, the largest, longest, most beautiful, but also the harshest mountain in Dalmatia. The path normally leads to the peak of Sveti Jure (1594 m), the highest peak of Biokovo, and our path goes to the peak of Vosac (1422 m). At the very beginning, the trail is wide and leads along serpentines, winding continuously. The higher the trail climbs, the narrower it gets. On the top of Vosac is the Toni Roso Mountain Hut, from where you can enjoy a wonderful view in all directions.	Hard	10.6	1191.0	8
 10	Mala Ucka - Vojak	The trail starts from the village of Mala Ucka, the highest rural settlement in Istria, located at 995 meters above sea level. The village is almost deserted. The path continues through a tall beech forest with a sharp climb to the exit to the saddle at the foot of the Vojak peak, from where there is a sharp ascent along a grassy rocky ridge all the way to the top where the old lookout tower is located, offering a beautiful view in all directions. The return goes to the saddle and the circular path back to Mala Ucka.	Moderate	6.9	429.0	9
 \.
@@ -249,13 +227,6 @@ COPY public.trails (trail_id, trail_name, trail_description, difficulty, length,
 --
 
 SELECT pg_catalog.setval('public.hut_id_seq', 10, true);
-
-
---
--- Name: mountainhuts_hutid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.mountainhuts_hutid_seq', 19, true);
 
 
 --
@@ -287,14 +258,6 @@ SELECT pg_catalog.setval('public.trails_trailid_seq', 1, false);
 
 
 --
--- Name: mountainhuts mountainhuts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.mountainhuts
-    ADD CONSTRAINT mountainhuts_pkey PRIMARY KEY (hut_id);
-
-
---
 -- Name: mountains mountains_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -308,14 +271,6 @@ ALTER TABLE ONLY public.mountains
 
 ALTER TABLE ONLY public.trails
     ADD CONSTRAINT trails_pkey PRIMARY KEY (trail_id);
-
-
---
--- Name: mountainhuts mountainhuts_mountainid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.mountainhuts
-    ADD CONSTRAINT mountainhuts_mountainid_fkey FOREIGN KEY (mountain_id) REFERENCES public.mountains(mountain_id);
 
 
 --
